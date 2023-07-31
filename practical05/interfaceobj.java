937c7f4ec32e87b811e825e78d3502717a6421dc
heshansilva <135937174+heshansilva@users.noreply.github.com>
2023-07-31 16:28:54 +0530

    Create interfaceobj.java

---
+++ practical05/interfaceobj.java
@@ -0,0 +1,14 @@
+package com.mycompany.interfaceobj;
+public class InterfaceObj 
+{
+    public static void main(String[] args)
+    {
+        Politician p1=new Politician();
+        Priest pr1=new Priest();
+        Lecturer l1=new Lecturer();
+        
+        p1.speak();
+        pr1.speak();
+        l1.speak();
+    }
+}
